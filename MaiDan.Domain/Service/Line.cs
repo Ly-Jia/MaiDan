6e640786d28bca5a0b4f84ea40c51ac317424f70
Ly-Jia Goldstein <hello@lyjia.net>
2014-12-18 22:15:40 +0100

    Service - Comparing two lines of an order (Equals)
    Two lines are equal if their quantities and dishnames are the same

--- MaiDan.Domain/Service/Line.cs
+++ MaiDan.Domain/Service/Line.cs
@@ -7,8 +7,22 @@ namespace MaiDan.Domain.Service
 	/// </summary>
 	public class Line
 	{
+		public int Quantity;
+		public String DishName;
+		
 		public Line(int quantity, String dishName)
 		{
+			Quantity = quantity;
+			DishName = dishName;
 		}
+		
+		public override bool Equals(object obj)
+		{
+			Line other = obj as Line;
+			if (other == null)
+				return false;
+			return this.Quantity == other.Quantity && this.DishName == other.DishName;
+		}
+
 	}
 }
