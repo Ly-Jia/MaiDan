51c6907dd7f26fdf48577bce91afb195451add38
Ly-Jia Goldstein <git@lyjia.net>
2015-02-03 21:09:19 +0100

    Unit tests refactoring

--- Test.MaiDan.Business/WaiterTest.cs
+++ Test.MaiDan.Business/WaiterTest.cs
@@ -18,8 +18,8 @@ namespace Test.MaiDan.Business
 		[Test]
 		public void can_take_an_order_from_a_date()
 		{
-			var orderBook = new Mock<IRepository<Order>>();
-			var waiter = new Waiter(orderBook.Object);
+		    var orderBook = new Mock<IRepository<Order>>();
+		    var waiter = new Waiter(orderBook.Object);
 			var order = new AnOrder().Build();
 			
 			waiter.Take(order);
@@ -30,8 +30,7 @@ namespace Test.MaiDan.Business
         [Test]
         public void should_not_add_a_dish_to_a_missing_order()
 	    {
-	        var orderBook = new Mock<IRepository<Order>>();
-	        var waiter = new Waiter(orderBook.Object);
+            var waiter = new AWaiter().Build();
 
             Assert.Throws<ItemNotFoundException>(() => waiter.AddDishToAnOrder(new DateTime(), 2, "Fried rice"));
 	    }
@@ -39,7 +38,7 @@ namespace Test.MaiDan.Business
 	    [Test]
 	    public void can_update_an_order()
 	    {
-	        var orderBook = new Mock<IRepository<Order>>();
+            var orderBook = new Mock<IRepository<Order>>();
             var waiter = new Waiter(orderBook.Object);
 
 	        var updatedOrder = new AnOrder().Build();
---
+++ Test.MaiDan/Service/AWaiter.cs
@@ -0,0 +1,22 @@
+﻿using MaiDan.Business;
+using MaiDan.DAL;
+using MaiDan.Domain.Service;
+using Moq;
+
+namespace Test.MaiDan.Service
+{
+    public class AWaiter
+    {
+        private Mock<IRepository<Order>> OrderBook;
+
+        public AWaiter()
+        {
+            OrderBook = new Mock<IRepository<Order>>();
+        }
+
+        public Waiter Build()
+        {
+            return new Waiter(OrderBook.Object);
+        }
+    }
+}
