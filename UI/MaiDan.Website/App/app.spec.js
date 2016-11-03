3e3aa48c32c27e983fd8da38ec33e2a1ffe616bd
Ly-Jia Goldstein <hello@lyjia.net>
2016-11-03 14:35:46 +0100

    [Service - UI] Show menu - static list of dishes
    Angular setup
    https://github.com/Ly-Jia/MaiDan/issues/9

---
+++ UI/MaiDan.Website/App/app.spec.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+describe('MenuController', function () {
+
+    beforeEach(module('maidanApp'));
+
+    it('should create a `menu` model with 3 dishes', inject(function ($controller) {
+        var scope = {};
+        var ctrl = $controller('MenuController', { $scope: scope });
+
+        expect(scope.menu.length).toBe(3);
+    }));
+
+});
