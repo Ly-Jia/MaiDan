8c18074094cd02614f637fca3a80e395fb4b3506
Thomas Goldstein <stifu@free.fr>
2018-03-31 12:15:20 +0200

    Restore the port 8888 setting in Program.cs

--- UI/MaiDan.Website/Program.cs
+++ UI/MaiDan.Website/Program.cs
@@ -19,6 +19,7 @@ namespace MaiDan.Website
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:8888");
     }
 }
