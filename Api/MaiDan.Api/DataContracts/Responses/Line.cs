6306215a616fcc6538ce12b1da1795eafc9f68f0
Thomas Goldstein <stifu@free.fr>
2018-04-13 21:04:06 +0200

    Add Id and DishId properties to Responses.Line

--- Api/MaiDan.Api/DataContracts/Responses/Line.cs
+++ Api/MaiDan.Api/DataContracts/Responses/Line.cs
@@ -4,12 +4,16 @@
     {
         public Line(Ordering.Domain.Line orderingLine, Billing.Domain.Line billingLine)
         {
+            Id = orderingLine.Id;
             Quantity = orderingLine.Quantity;
+            DishId = orderingLine.Dish.Id;
             DishName = orderingLine.Dish.Name;
             Amount = billingLine.Amount;
         }
 
+        public int Id { get; set; }
         public int Quantity { get; set; }
+        public string DishId { get; set; }
         public string DishName { get; set; }
         public decimal Amount { get; set; }
     }
--- UI/MaiDan.Website/ClientApp/src/app/shared/models/order-line.ts
+++ UI/MaiDan.Website/ClientApp/src/app/shared/models/order-line.ts
@@ -1,5 +1,7 @@
 export class OrderLine {
+    id: number;
     quantity: number;
+    dishId: string;
     dishName: string;
     amount: number;
   }
