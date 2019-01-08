6d61a227e9cad8c0e5dd1c4f97feea9756e5faec
Thomas Goldstein <stifu@free.fr>
2019-01-09 00:17:52 +0100

    Reformatting

--- Test/Ordering/Test.MaiDan.Ordering/AnOrder.cs
+++ Test/Ordering/Test.MaiDan.Ordering/AnOrder.cs
@@ -4,105 +4,103 @@ using MaiDan.Ordering.Domain;
 
 namespace Test.MaiDan.Ordering
 {
-	/// <summary>
-	/// Builder that creates Order
-	/// </summary>
-	public class AnOrder
-	{
-		public static readonly int DEFAULT_ID = 1;
-		private int id;
-		private List<Line> lines;
-	    private bool takeAway = false;
-	    private int numberOfGuests = 0;
-	    private Table table;
+    /// <summary>
+    /// Builder that creates Order
+    /// </summary>
+    public class AnOrder
+    {
+        public static readonly int DEFAULT_ID = 1;
+        private int id;
+        private List<Line> lines;
+        private bool takeAway = false;
+        private int numberOfGuests = 0;
+        private Table table;
         private Table defaultTable = new Table("1");
-	    private DateTime orderingDate = new DateTime(2018, 08, 08);
-	    private bool closed = false;
-		
-		/// <summary>
-		/// Initialize the future order with a specific id (creation date)
-		/// </summary>
-		/// <param name="id"></param>
-		public AnOrder(int id)
-		{
+        private DateTime orderingDate = new DateTime(2018, 08, 08);
+        private bool closed = false;
+
+        /// <summary>
+        /// Initialize the future order with a specific id (creation date)
+        /// </summary>
+        /// <param name="id"></param>
+        public AnOrder(int id)
+        {
             this.lines = new List<Line>();
-		    this.id = id;
-		}
-		
-		/// <summary>
-		/// Initialize the order with a default id (DateTime : 2012/12/21)
-		/// </summary>
-		public AnOrder():this(DEFAULT_ID)
-		{
-		}
+            this.id = id;
+        }
 
-	    public AnOrder OnSite(string tableId, int guests)
-	    {
-	        takeAway = false;
+        /// <summary>
+        /// Initialize the order with a default id (DateTime : 2012/12/21)
+        /// </summary>
+        public AnOrder() : this(DEFAULT_ID)
+        {
+        }
+
+        public AnOrder OnSite(string tableId, int guests)
+        {
+            takeAway = false;
             table = new Table(tableId);
-	        numberOfGuests = guests;
-	        return this;
-	    }
+            numberOfGuests = guests;
+            return this;
+        }
 
-	    public AnOrder OnSite(Table t, int guests)
-	    {
-	        takeAway = false;
-	        table = t;
-	        numberOfGuests = guests;
-	        return this;
-	    }
+        public AnOrder OnSite(Table t, int guests)
+        {
+            takeAway = false;
+            table = t;
+            numberOfGuests = guests;
+            return this;
+        }
 
         public AnOrder TakeAway()
-	    {
-	        takeAway = true;
-	        return this;
-	    }
-        
-		/// <summary>
-		/// Add a new line to the created order
-		/// </summary>
-		/// <param name="quantity"></param>
-		/// <param name="dish"></param>
-		/// <returns></returns>
-		public AnOrder With(int quantity, Dish dish)
-		{
-			return With(new Line (this.lines.Count, quantity, dish));
-		}
-		
-		/// <summary>
-		/// Add a new line to the created order
-		/// </summary>
-		/// <param name="line"></param>
-		/// <returns></returns>
-		public AnOrder With(Line line)
-		{
-			lines.Add(line);
-			return this;
-		}
+        {
+            takeAway = true;
+            return this;
+        }
 
-	    public AnOrder AlreadyPrinted()
-	    {
-	        closed = true;
-	        return this;
+        /// <summary>
+        /// Add a new line to the created order
+        /// </summary>
+        /// <param name="quantity"></param>
+        /// <param name="dish"></param>
+        /// <returns></returns>
+        public AnOrder With(int quantity, Dish dish)
+        {
+            return With(new Line(this.lines.Count, quantity, dish));
+        }
 
-	    }
+        /// <summary>
+        /// Add a new line to the created order
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        public AnOrder With(Line line)
+        {
+            lines.Add(line);
+            return this;
+        }
 
-	    public AnOrder And(int quantity, Dish dish)
-	    {
-	        return With(quantity, dish); 
-	    }
-		
-		/// <summary>
-		/// Instanciate the order
-		/// </summary>
-		/// <returns></returns>
-		public Order Build()
-		{
+        public AnOrder AlreadyPrinted()
+        {
+            closed = true;
+            return this;
+        }
+
+        public AnOrder And(int quantity, Dish dish)
+        {
+            return With(quantity, dish);
+        }
+
+        /// <summary>
+        /// Instanciate the order
+        /// </summary>
+        /// <returns></returns>
+        public Order Build()
+        {
             if (takeAway)
-			    return new TakeAwayOrder(id, orderingDate, lines, false);
+                return new TakeAwayOrder(id, orderingDate, lines, false);
 
             return new OnSiteOrder(id, table ?? defaultTable, numberOfGuests, orderingDate, lines, closed);
-		}
-        
-	}
+        }
+    }
 }
